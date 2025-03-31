77dd92474d7589c89faf1b348aa7ed35f0154f69
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-03-31 20:55:55 +0530

    Time: 68 ms (2.55%) | Memory: 44.7 MB (10.30%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,33 @@
+public class Solution {
+
+  boolean isValidBST(TreeNode root) {  // Renamed to isValidBST
+
+    List<Integer> inOrderList = new LinkedList<>();
+
+    // Populate the list
+    helper(root, inOrderList);
+
+    boolean isBST = true;
+    int prev = inOrderList.get(0);
+    for (int i = 1; i < inOrderList.size(); i++) {
+
+      // Check if new element is smaller than previous element
+      // or if the element is duplicate
+      if (inOrderList.get(i) <= prev)
+        isBST = false;
+
+      prev = inOrderList.get(i);
+    }
+
+    return isBST;
+  }
+
+  void helper(TreeNode treeNode, List<Integer> inOrderList) {
+    if (treeNode == null)
+      return;
+
+    helper(treeNode.left, inOrderList);
+    inOrderList.add(treeNode.val);
+    helper(treeNode.right, inOrderList);
+  }
+}
