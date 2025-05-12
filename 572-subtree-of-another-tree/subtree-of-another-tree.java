c61fecccfcb51b4251e8d9015ffc3d014b135baa
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-05-12 14:04:19 +0530

    Time: 7 ms (7.84%) | Memory: 44.8 MB (20.75%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,28 +13,58 @@
  *     }
  * }
  */
-class Solution {
-    public static boolean isIdentical(TreeNode node, TreeNode subRoot) {
-        if (node == null && subRoot == null) {
-            return true;
-        } else if (node == null || subRoot == null || node.val != subRoot.val) {
-            return false;
+
+
+
+
+ //RECURSIVE SOLUTION
+// class Solution {
+//     public static boolean isIdentical(TreeNode node, TreeNode subRoot) {
+//         if (node == null && subRoot == null) {
+//             return true;
+//         } else if (node == null || subRoot == null || node.val != subRoot.val) {
+//             return false;
+//         }
+
+//         return isIdentical(node.left, subRoot.left) && isIdentical(node.right, subRoot.right);
+//     }
+
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         if (root == null) {
+//             return false;
+//         }
+
+//         if (root.val == subRoot.val) {
+//             if (isIdentical(root, subRoot)) {
+//                 return true;
+//             }
+//         }
+
+//         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//     }
+// }
+
+
+//ITERATIVE SOLUTION 
+public class Solution{
+    String preOrderTraversal(TreeNode node){
+        if(node==null){
+            return "null";
         }
 
-        return isIdentical(node.left, subRoot.left) && isIdentical(node.right, subRoot.right);
+        StringBuilder sb=new StringBuilder("^");
+        sb.append(node.val);
+        sb.append(preOrderTraversal(node.left));
+        sb.append(preOrderTraversal(node.right));
+
+        return sb.toString();
     }
 
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) {
-            return false;
-        }
+     boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
-        if (root.val == subRoot.val) {
-            if (isIdentical(root, subRoot)) {
-                return true;
-            }
-        }
+    String fullTree = preOrderTraversal(root);
+    String subTree = preOrderTraversal(subRoot);
 
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    return (fullTree.contains(subTree));
     }
 }
