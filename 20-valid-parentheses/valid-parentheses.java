a5695c498359ca0cf26d9d828614a1a00e603ba6
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-05-23 00:19:39 +0530

    Time: 2 ms (97.22%) | Memory: 41.9 MB (32.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,58 @@
-class Solution {
-    public boolean isValid(String s) {
-    Stack<Character> stack = new Stack<>();
 
-    for (char c : s.toCharArray()) {
+//  aproach 1 
 
-        if (c == '(')
-            stack.push(')');
-        else if (c == '{')
-            stack.push('}');
-        else if (c == '[')
-            stack.push(']');
+// class Solution {
+//     public boolean isValid(String s) {
+//     Stack<Character> stack = new Stack<>();
 
-        else if (stack.isEmpty() || stack.pop() != c)
-            return false;
+//     for (char c : s.toCharArray()) {
+
+//         if (c == '(')
+//             stack.push(')');
+//         else if (c == '{')
+//             stack.push('}');
+//         else if (c == '[')
+//             stack.push(']');
+
+//         else if (stack.isEmpty() || stack.pop() != c)
+//             return false;
+//     }
+//     return stack.isEmpty();
+//     }
+// }
+
+
+
+// aproach 2
+class Solution{
+    
+    public char getVal(char ch){
+        switch(ch){
+            case ']' : return '[';
+            case '}' : return '{';
+            case ')' : return '(';
+            default  : return ' '; 
+        }
     }
-    return stack.isEmpty();
+
+
+    public boolean isValid(String s){
+        String opening="({[";
+        String closing="]})";
+
+        Stack<Character> st=new Stack<>();
+        
+        for(char ch : s.toCharArray()){
+            if(opening.indexOf(ch)!=-1)
+            st.push(ch);
+            else{
+                if(st.size()==0) return false;
+                char tmp=st.pop();
+                if(getVal(ch)!=tmp)
+                return false;
+            }
+
+        }
+        return st.size()==0;
     }
 }
