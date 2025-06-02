6c5aaefd36a27d621419c193259a9ebdf8c68729
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-06-02 12:10:22 +0530

    Time: 3 ms (84.24%) | Memory: 49.9 MB (39.52%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count =0;
+        int n=grid.length;
+        int m=grid[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    solve(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count++;
+    }
+
+    public void solve(char [][] grid,int i,int j){
+        //dead ends
+        if(i<0 || j<0 || i>=grid.length || j>=grid[i].length || grid[i][j]=='0' || grid[i][j]=='2'){
+            return;
+        }
+
+        grid[i][j] = '2';
+
+        solve(grid,i+1,j);
+        solve(grid,i,j+1);
+        solve(grid,i-1,j);
+        solve(grid,i,j-1);
+    }
+}
