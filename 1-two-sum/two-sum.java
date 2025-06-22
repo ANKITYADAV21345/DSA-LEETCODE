6bfd79af21e20b653501bc38c3cd693d51b2bd1c
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-06-22 16:54:38 +0530

    Time: 2 ms (98.93%) | Memory: 45 MB (46.93%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a hashmap to store the number and its index
-        Map<Integer, Integer> map = new HashMap<>();
         
-        // Traverse through the array
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            // Check if complement exists in the map
-            if (map.containsKey(complement)) {
-                // If yes, return the indices
-                return new int[] {map.get(complement), i};
+        //create hashmap
+        Map <Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+
+            //get the complement using the target value 
+            int complement=target - nums[i];
+
+            //search the hashmap for complement ,if found ,we got our pair
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
-            
-            // If not, add the current number with its index to the map
-            map.put(nums[i], i);
+
+            //put the element in hashmap for subsequent searches 
+            map.put(nums[i],i);
         }
-        
-        // If no solution is found (although the problem guarantees a solution)
-        return new int[] {-1, -1};
+        return new int []{0,0};
     }
 }
-// apna college vali mam ne sorted array ke liye karaya hai and isme sorted array nahi hain
