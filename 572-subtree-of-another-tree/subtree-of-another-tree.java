4b576e35e2846aff43889a902c1d9a39ebc2d414
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-07-16 22:46:20 +0530

    Time: 9 ms (5.12%) | Memory: 45.7 MB (6.32%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,56 +15,25 @@
  */
 
 
-
-
- //RECURSIVE SOLUTION
-// class Solution {
-//     public static boolean isIdentical(TreeNode node, TreeNode subRoot) {
-//         if (node == null && subRoot == null) {
-//             return true;
-//         } else if (node == null || subRoot == null || node.val != subRoot.val) {
-//             return false;
-//         }
-
-//         return isIdentical(node.left, subRoot.left) && isIdentical(node.right, subRoot.right);
-//     }
-
-//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-//         if (root == null) {
-//             return false;
-//         }
-
-//         if (root.val == subRoot.val) {
-//             if (isIdentical(root, subRoot)) {
-//                 return true;
-//             }
-//         }
-
-//         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-//     }
-// }
-
-
-//ITERATIVE SOLUTION 
-public class Solution{
-    String preOrderTraversal(TreeNode node){
-        if(node==null){
+ //nikhil lohia
+class Solution {
+    // Serialize the tree using preorder traversal with null markers
+    private String preOrderTraversal(TreeNode node) {
+        if (node == null) {
             return "null";
         }
 
-        StringBuilder sb=new StringBuilder("^");
-        sb.append(node.val);
-        sb.append(preOrderTraversal(node.left));
+        StringBuilder sb = new StringBuilder();
+        sb.append("#").append(node.val).append(" "); // Use # to separate nodes safely
+        sb.append(preOrderTraversal(node.left)).append(" ");
         sb.append(preOrderTraversal(node.right));
 
         return sb.toString();
     }
 
-     boolean isSubtree(TreeNode root, TreeNode subRoot) {
-
-    String fullTree = preOrderTraversal(root);
-    String subTree = preOrderTraversal(subRoot);
-
-    return (fullTree.contains(subTree));
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        String fullTree = preOrderTraversal(root);
+        String subTree = preOrderTraversal(subRoot);
+        return fullTree.contains(subTree);
     }
 }
