4a5164a9e7b872d537a9ee0a8aca40cafd1712f0
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-07-17 16:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (80.44%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,37 +1,19 @@
+//codebix
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
+        int lo = 0, hi = n - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if ((mid % 2 == 0 && mid + 1 < n && nums[mid] == nums[mid + 1]) ||
+                    (mid % 2 == 1 && mid - 1 >= 0 && nums[mid] == nums[mid - 1]))
+                lo = mid + 1;
+            else
+                hi = mid;
 
-        if (n == 1) return nums[0];
-
-        int st = 0, end = n - 1;
-
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            if (mid == 0 && nums[0] != nums[1]) return nums[mid];
-            
-            if (mid == n - 1 && nums[n - 1] != nums[n - 2]) return nums[mid];
-
-            if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) return nums[mid];
-
-            if (mid % 2 == 0) { // even
-                if (nums[mid] == nums[mid + 1]) {
-                    st = mid + 2;
-                } else {
-                    end = mid - 1;
-                }
-            } 
-            
-            else { // odd
-                if (nums[mid] == nums[mid - 1]) {
-                    st = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
+            if (mid - 1 >= 0 && mid + 1 < n && nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                return nums[mid];
         }
-
-        return -1;
+        return nums[lo];
     }
 }
