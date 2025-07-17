57434b562fde8735172580ed0ce45b50732e27cc
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-07-17 16:42:56 +0530

    Time: 1 ms (91.25%) | Memory: 45.2 MB (21.75%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,36 +15,40 @@
  */
 
 
- //not much optimised
+ //codebix
 import java.util.*;
 
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        q.add(null); // Level delimiter
-
-        List<Integer> level = new ArrayList<>();
-
-        while (!q.isEmpty()) {
-            TreeNode currNode = q.remove();
-
-            if (currNode == null) {
-                result.add(new ArrayList<>(level));
-                level.clear();
-                if (!q.isEmpty()) {
-                    q.add(null);
-                }
-            } else {
-                level.add(currNode.val);
-                if (currNode.left != null) q.add(currNode.left);
-                if (currNode.right != null) q.add(currNode.right);
-            }
+        List<List<Integer>> output = new ArrayList<>();
+        
+        if (root == null) {
+            return output;
         }
 
-        return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> list1 = new ArrayList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list1.add(node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            output.add(list1);
+        }
+
+        return output;
     }
 }
