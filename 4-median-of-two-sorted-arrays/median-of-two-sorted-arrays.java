8f190b9eec7267f864ab7d7029c62ee1a3c36b2e
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-08-21 01:26:42 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (98.98%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 class Solution {
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //MAKE num1 as min length array
+        //call same function and change the params
+        //if nums1 was greater and nums2 was smaller 
+        //so because e swaped these two now nums1 is smaller and 
+        //nums2 is greater 
         // Ensure nums1 is the smaller array
         if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
@@ -22,10 +27,13 @@ class Solution {
             int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
             int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
 
+            // cheak if cut is valid
             if (l1 <= r2 && l2 <= r1) {
+                //if total length is even only 2 ans is possible take avg
                 if (totalLength % 2 == 0) {
                     return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
                 } else {
+                    // total length is odd then 1 values
                     return Math.max(l1, l2);
                 }
             } else if (l1 > r2) {
