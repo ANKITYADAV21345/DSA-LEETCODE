e774b711b05a3a7b95a46fe26057568be9a1bf9d
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-08-22 00:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (48.31%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
@@ -13,29 +13,30 @@
  *     }
  * }
  */
+
+//saurin majumdar
 class Solution {
-
-    public void inorder(TreeNode root,List<Integer> inorder){
-        if(root==null) return;
-
-        inorder(root.left,inorder);
-        inorder.add(root.val);
-        inorder(root.right,inorder);
-    }
-
-
+    private int minDiff = Integer.MAX_VALUE;
+    private TreeNode prev = null;
 
     public int minDiffInBST(TreeNode root) {
-        
-        List<Integer> inorder=new ArrayList<>();
+        inorder(root);
+        return minDiff;
+    }
 
-        inorder(root,inorder);
-
-        int min =Integer.MAX_VALUE;
-
-        for(int i=1;i<inorder.size();i++){
-            min =Math.min(min,inorder.get(i)-inorder.get(i-1));
+    private void inorder(TreeNode root) {
+        if (root == null) {
+            return;
         }
-        return min;
+
+        inorder(root.left);
+
+        if (prev != null) {
+            int diff = Math.abs(prev.val - root.val);
+            minDiff = Math.min(minDiff, diff);
+        }
+
+        prev = root;
+        inorder(root.right);
     }
 }
