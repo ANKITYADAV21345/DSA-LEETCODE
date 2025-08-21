67ad6b1e555046d8d26fb891ca66b91aaff92188
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-08-22 00:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (22.63%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -14,94 +14,17 @@
  * }
  */
 
-//methord 1   RECURSIVE APPROACH     TC  O(N)  SC  O(N)
-// class Solution {
-//     private int globalSum;
 
-//     private void solve(TreeNode node) {
-//         if (node == null) return;
-
-//         solve(node.right);
-//         globalSum += node.val;
-//         node.val = globalSum; // update the node's value
-//         solve(node.left);
-//     }
-
-//     public TreeNode bstToGst(TreeNode root) {
-//         globalSum = 0;
-//         solve(root);
-//         return root;
-//     }
-// }
-
-
-
-// //METHORD 2  ITERATIVE APROACH   TC  O(N)    SC O(N)
-// class Solution {
-//     private int globalSum;
-
-//     public TreeNode bstToGst(TreeNode root){
-//         globalSum=0;
-//         Stack<TreeNode> stack=new Stack<>();
-//         TreeNode node = root;
-
-//         while(!stack.isEmpty()||node!=null){
-//             while(node!=null){
-//                 stack.push(node);
-//                 node=node.right;
-//             }
-//             node=stack.pop();
-//             globalSum=globalSum+node.val;
-//             node.val=globalSum;
-
-//             node=node.left;
-//         }
-//         return root;
-//     }
-// }
-
-
-
-
-
-//aproach 3 by morish traversal TC O(N)  SC  O(1)
+//ayushi sharma
 class Solution {
-    private int globalSum;
-
-    private TreeNode postExtremeLeftFunc(TreeNode node){
-        TreeNode post=node.right;
-        while(post.left!=null && post.left!=node){
-            post=post.left;
-        }
-        return post;
-    }
-
-
-    public TreeNode bstToGst(TreeNode root){
-        globalSum=0;
-        TreeNode node=root;
-
-        while(node!=null){
-            TreeNode post=node.right;
-            if(post==null){
-                globalSum=globalSum+node.val;
-                node.val=globalSum;
-                node=node.left;
-            }
-            else{
-                TreeNode postExtremeLeft=postExtremeLeftFunc(node);
-
-                if(postExtremeLeft.left==null){
-                    postExtremeLeft.left=node;
-                    node=node.right;
-                }
-                else{
-                    postExtremeLeft.left=null;
-                    globalSum=globalSum+node.val;
-                    node.val=globalSum;
-                    node=node.left;
-                }
-            }
+    private int sum = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {  
+        if (root != null) {
+            bstToGst(root.right); 
+            root.val += sum;
+            sum = root.val; 
+            bstToGst(root.left);
         }
         return root;
     }
