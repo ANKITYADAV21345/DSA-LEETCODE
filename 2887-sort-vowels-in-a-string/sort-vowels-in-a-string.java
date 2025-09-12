3c69d089d823318a50e0fda6ac87b3fde8ccb4bd
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-09-12 11:56:43 +0530

    Time: 26 ms (82.05%) | Memory: 46.1 MB (78.31%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -2,19 +2,18 @@ class Solution {
     public String sortVowels(String s) {
         char[] result=s.toCharArray();
         char[] temp=new char[s.length()];
+        HashSet<Character> vowels= new HashSet<>(Arrays.asList('A','E','I','O','U','a','e','i','o','u'));
         int k=0,j=0;
-        for(int i=0;i<s.length();i++){
-            if(result[i]=='A'||result[i]=='E'||result[i]=='I'|| result[i]=='O'||result[i]=='U'||
-                result[i]=='a'|| result[i]=='e'||result[i]=='i'||result[i]=='o'||result[i]=='u'){
-                    temp[k++]=result[i];
+        for(char c:result){
+            if(vowels.contains(c)){
+                    temp[k++]=c;
                 }
         }
         Arrays.sort(temp,0,k);
         //values update kr rahe hai
         StringBuilder ans=new StringBuilder();
         for(char c:result){
-            if(c=='A'||c=='E'||c=='I'|| c=='O'||c=='U'||
-                c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+            if(vowels.contains(c)){
                     ans.append(temp[j++]);
             }
             else{
