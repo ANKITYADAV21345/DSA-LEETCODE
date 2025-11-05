9fbcee08bf943cb10bf4a29db282b685d47cb09b
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-05 20:01:55 +0530

    Time: 1 ms (99.91%) | Memory: 46.8 MB (5.19%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,23 +9,52 @@
  * }
  */
 
-//neetcode 150 by destination fang part 1   time stramp 10:36:55
+//algorithm made easy
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null||lists.length==0) return null;
         
-        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
-        for(ListNode list:lists){
-            while(list!=null){
-                minHeap.add(list.val);
-                list=list.next;
+        int interval=1;
+
+        while(interval<lists.length){
+            for(int i=0;i+interval<lists.length;i=i+interval*2){
+                lists[i]=mergeTwoLists(lists[i],lists[i+interval]);
             }
+            interval=interval*2;
         }
-        ListNode dummy=new ListNode(1);
-        ListNode merge=dummy=dummy;
-        while(!minHeap.isEmpty()){
-            merge.next=new ListNode(minHeap.remove());
-            merge=merge.next;
+
+        return lists[0];
+    }
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a sentinel/dummy node to start
+        ListNode returnNode = new ListNode(Integer.MIN_VALUE);
+
+        // Create a copy of this node to iterate while solving the problem
+        ListNode headNode = returnNode;
+
+        // Traverse till one of the list reaches the end
+        while (list1 != null && list2 != null) {
+            // Compare the 2 values of lists
+            if (list1.val <= list2.val) {
+                returnNode.next = list1;
+                list1 = list1.next;
+            } else {
+                returnNode.next = list2;
+                list2 = list2.next;
+            }
+            returnNode = returnNode.next;
         }
-        return dummy.next;
+
+        // Append the remaining list
+        if (list1 == null) {
+            returnNode.next = list2;
+        } else {
+            returnNode.next = list1;
+        }
+
+        // Return the next node to sentinel node
+        return headNode.next;
     }
 }
