d462d81add7522c46b16d7ee41bf2b4f72ee391c
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-11 14:51:24 +0530

    Time: 71 ms (24.96%) | Memory: 169.2 MB (5.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,37 @@
-//nikhil lohia
+//apna college notes
+import java.util.*;
+
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return 0;
+    public int lengthOfLIS(int[] nums1) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
 
-        int n = nums.length;
-        int[] T = new int[n];
-        Arrays.fill(T, 1);  // Each element is an LIS of length 1
+        int[] nums2 = new int[set.size()];
+        int i = 0;
+        for (int val : set) {
+            nums2[i++] = val;
+        }
+        Arrays.sort(nums2);
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    T[i] = Math.max(T[i], T[j] + 1);
+        return lcs(nums1, nums2);
+    }
+
+    public static int lcs(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        // Find the maximum value in T
-        int maxLen = 0;
-        for (int len : T) {
-            maxLen = Math.max(maxLen, len);
-        }
-
-        return maxLen;
+        return dp[n][m];
     }
 }
