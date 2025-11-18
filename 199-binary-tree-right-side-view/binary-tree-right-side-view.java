5a1759c9aecae1d8dfb008a407b9de40be0ebb83
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-19 00:08:31 +0530

    Time: 2 ms (5.61%) | Memory: 43.9 MB (5.82%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,31 +13,54 @@
  *     }
  * }
  */
+
+
+
+ //apna college notes(using hashmap thoda logic tweek)
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-    if (root == null) return result;
 
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.offer(root);
+    static class info{
+        TreeNode node;
+        int level;
 
-    while (!queue.isEmpty()) {
-      int levelSize = queue.size();  // Number of nodes at the current level
-      int lastValue = 0;  // Store the last node's value of the level
-
-      for (int i = 0; i < levelSize; i++) {
-        TreeNode node = queue.poll();
-        lastValue = node.val;  // Update lastValue with the current node
-
-        // Add child nodes to queue for next level processing
-        if (node.left != null) queue.offer(node.left);
-        if (node.right != null) queue.offer(node.right);
-      }
-
-      result.add(lastValue);  // Add the last node's value of this level
+        info(TreeNode node,int level){
+            this.node=node;
+            this.level=level;
+        }
     }
 
-    return result;
-   }
-    
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+
+        if(root==null){
+            return ans;
+        }
+
+        Queue<info> q=new LinkedList<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        q.add(new info(root,0));
+        int maxLevel=0;
+
+        while(!q.isEmpty()){
+            info cur=q.remove();
+            TreeNode node=cur.node;
+            int level=cur.level;
+
+            map.put(level,node.val);
+            maxLevel=Math.max(maxLevel,level);
+
+            if(node.left!=null){
+                q.add(new info(node.left,level+1));
+            }
+            if(node.right!=null){
+                q.add(new info(node.right,level+1));
+            }
+        }
+        for(int i=0;i<=maxLevel;i++){
+            ans.add(map.get(i));
+        }
+        return ans;
+    }
 }
