499641041d9ed35d61b9c7c0fdb6a9d6697478d2
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-21 01:28:44 +0530

    Time: 2 ms (97.31%) | Memory: 48.7 MB (7.66%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -13,36 +13,43 @@
  *     }
  * }
  */
-//nikhil lohia
+
+
+
+
+
+//done by own (recursion) isme arraylist ka set function ache se use hua hai
 class Solution {
+
+    List<Double> levelSum = new ArrayList<>();
+    List<Integer> levelCount = new ArrayList<>();
+
     public List<Double> averageOfLevels(TreeNode root) {
-        Queue<TreeNode> levelQueue = new LinkedList<>();
-        levelQueue.add(root);
-        levelQueue.add(null);
+        fillLevelData(root, 0);
 
-        List<Double> avgList = new ArrayList<>();
-
-        while (levelQueue.peek() != null) {
-
-            double sum = 0;
-            int nodes = 0;
-
-            while (levelQueue.peek() != null) {
-
-                TreeNode node = levelQueue.poll();
-                sum += node.val;
-                nodes++;
-
-                if (node.left != null)
-                    levelQueue.add(node.left);
-                if (node.right != null)
-                    levelQueue.add(node.right);
-            }
-
-            levelQueue.add(levelQueue.poll());
-            avgList.add(sum / nodes);
+        List<Double> result = new ArrayList<>();
+        for (int i = 0; i < levelSum.size(); i++) {
+            result.add(levelSum.get(i) / levelCount.get(i));
         }
 
-        return avgList;
+        return result;
+    }
+
+    // Full name instead of dfs
+    private void fillLevelData(TreeNode node, int level) {
+        if (node == null) return;
+
+        // If this is the first time we reach this level
+        if (level == levelSum.size()) {
+            levelSum.add((double) node.val);
+            levelCount.add(1);
+        } 
+        else {
+            levelSum.set(level, levelSum.get(level) + node.val);
+            levelCount.set(level, levelCount.get(level) + 1);
+        }
+
+        fillLevelData(node.left, level + 1);
+        fillLevelData(node.right, level + 1);
     }
 }
