7deaa75c2d0fba30b54ce0c06a69550fbbca0298
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-24 14:33:34 +0530

    Time: 148 ms (25.70%) | Memory: 48.9 MB (5.35%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,33 +1,48 @@
+//done by own(tabular)
+
+//table
+//   0 1 2 3 4      
+// 0 T F T F F
+// 1   T F T F
+// 2     T F F
+// 3       T F
+// 4         T
 
 
-//bruteforce isme har eek palindromeic string ko cheak kar rahe hai o(n3) ja rahi thai time complexity 
-//editorial se liya hai ye solution
+
 class Solution {
     public String longestPalindrome(String s) {
-        for (int length = s.length(); length > 0; length--) {
-            for (int start = 0; start <= s.length() - length; start++) {
-                if (check(start, start + length, s)) {
-                    return s.substring(start, start + length);
+        int n=s.length();
+        boolean[][] dp=new boolean[n][n];
+        String ans="";
+        int maxLen=0;
+
+        //fill table from botom to top
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+
+                //case 1 single char
+                if(i==j){
+                    dp[i][j]=true;
+                }
+
+                //two characters  (ADJACENT CHARACTER CHEAK WEATHER SAME OR NOT)
+                else if(j-i==1 && s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=true;
+                }
+
+                //case 3 length3 or more 
+                else if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]){
+                    dp[i][j]=true;
+                }
+
+                //TRACK longest
+                if(dp[i][j]&& (j-i+1>maxLen)){
+                    maxLen=j-i+1;
+                    ans=s.substring(i,j+1);  // j+1 iss liye kyoki substring me last index include nahi hota
                 }
             }
         }
-
-        return "";
-    }
-
-    private boolean check(int i, int j, String s) {
-        int left = i;
-        int right = j - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
+        return ans;
     }
 }
