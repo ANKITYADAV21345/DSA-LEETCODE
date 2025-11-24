0e752a1d58a89aa6c6dc0e4960ef107f0ed477bf
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-24 12:01:08 +0530

    Time: 53 ms (34.63%) | Memory: 187.6 MB (5.35%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,33 +1,50 @@
+//done by mu own (recursion+memoisation)
 class Solution {
-    public boolean isPalindrome(String s,int i,int j) {
-        
 
-        while(i<j){
-            char ch1=s.charAt(i);
-            char ch2=s.charAt(j);
-            if(ch1!=ch2)
-                return false;
-            i++;
-            j--;
+    //memo table
+    String[][] memo;
+
+    public String expand(String s,int left,int right){
+
+        //if already computed
+        if(left>=0 && right<s.length() && memo[left][right]!=null){
+            return memo[left][right];
         }
-        return true;
-    }
 
-    public String longestPalindrome(String s){
-        int max=0,start=0,end=0;
-        
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-             if(isPalindrome(s,i,j)==true){
-                if((j-i+1)>max){
-                    max=j-i+1;
-                    start=i;
-                    end=j;
-                }
-             }  
+        //base case boundry cross or mismatch
+        if(left<0 ||right>=s.length() ||s.charAt(left)!=s.charAt(right)){
+            return s.substring(left+1,right);
+        }
+
+        //recursively expand bothdirections
+        String ans=expand(s,left-1,right+1);
+
+        //store in memo(only if valid index)
+        if(left>=0 && right<s.length()){
+            memo[left][right]=ans;
+        }
+        return ans;
+    }
+    public String longestPalindrome(String s) {
+        memo=new String[s.length()][s.length()];
+        String longest="";
+
+        for(int i=0;i<s.length();i++){
+
+            //odd length centre
+            String odd=expand(s,i,i);
+
+            //even length centre
+            String even=expand(s,i,i+1);
+
+            //cheak max
+            if(odd.length()>longest.length()){
+                longest=odd;
+            }
+            if(even.length()>longest.length()){
+                longest=even;
             }
         }
-        return s.substring(start,end+1);
+        return longest;
     }
 }
