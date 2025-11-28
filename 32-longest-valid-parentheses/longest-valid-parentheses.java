f5f9cee09efdca71bd71cfa5d59eda5b27874802
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 03:21:12 +0530

    Time: 1 ms (99.80%) | Memory: 44.8 MB (80.88%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,14 +1,20 @@
+//done by my own tabular
 class Solution {
     public int longestValidParentheses(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
         int res = 0;
-        int dp[] = new int[s.length() + 1];
 
-        for (int i = 1; i < s.length(); i++) {
-            int j = i - dp[i] - 1;
-            if (s.charAt(i) == ')' && j >= 0 && s.charAt(j) == '(')
-                dp[i + 1] = dp[i] + dp[j] + 2;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                int prev = i - dp[i - 1] - 1;
 
-            res = Math.max(res, dp[i + 1]);
+                if (prev >= 0 && s.charAt(prev) == '(') {
+                    dp[i] = dp[i - 1] + 2;
+                    if (prev - 1 >= 0) dp[i] += dp[prev - 1];
+                }
+            }
+            res = Math.max(res, dp[i]);
         }
         return res;
     }
