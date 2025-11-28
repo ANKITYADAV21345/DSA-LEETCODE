fd6df14619fdfbd6cf953bac0f6ab9478549c1b2
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 03:59:48 +0530

    Time: 17 ms (73.17%) | Memory: 63.5 MB (6.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,40 +1,41 @@
-//apna college notes
+//recursion +tabular
 class Solution {
+
+    Boolean[][] memo;
     public boolean isMatch(String s, String p) {
-        int n=s.length();
-        int m=p.length();
+        memo = new Boolean[s.length()+1][p.length()+1];
+        return solve(s, p, s.length()-1, p.length()-1);
+    }
 
-        boolean dp[][]=new boolean[n+1][m+1];
-        //initialize
-        dp[0][0]=true;
+    private boolean solve(String s, String p, int i, int j) {
 
-        //patern ki length khali hai
-        for(int i=1;i<n+1;i++){
-            dp[i][0]=false;
-        }
+        if (i < 0 && j < 0) return true;
+        if (j < 0 && i >= 0) return false;
 
-        //string ki length khali hai 
-        for(int j=1;j<m+1;j++){
-            if(p.charAt(j-1)=='*'){
-                dp[0][j]=dp[0][j-1];
+        if (i < 0) {
+            for (int k = 0; k <= j; k++) {
+                if (p.charAt(k) != '*') return false;
             }
+            return true;
         }
 
-        //botom up code 
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                //case->ith char==jth char||jth char=='?'
-                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?'){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else if(p.charAt(j-1)=='*'){
-                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
-                }
-                else{
-                    dp[i][j]=false;
-                }
-            }
+        if (memo[i][j] != null) return memo[i][j];
+
+        boolean ans;
+        char cs = s.charAt(i);
+        char cp = p.charAt(j);
+
+        if (cs == cp || cp == '?') {
+            ans = solve(s, p, i-1, j-1);
         }
-        return dp[n][m];
+        else if (cp == '*') {
+            ans = solve(s, p, i-1, j)  // * consumes char
+               || solve(s, p, i, j-1); // * matches empty
+        }
+        else {
+            ans = false;
+        }
+
+        return memo[i][j] = ans;
     }
 }
