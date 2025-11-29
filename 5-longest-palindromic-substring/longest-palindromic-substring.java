174a63ac9d7c828b8994afef892e7800bdde288a
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 16:39:07 +0530

    Time: 170 ms (24.06%) | Memory: 42 MB (98.64%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,38 +1,26 @@
 class Solution {
-
-    String [][] memo;
-    public String expand(String s,int left ,int right){
-        if(left>=0 && right<s.length() && memo[left][right]!=null){
-            return memo[left][right];
+    public String longestPalindrome(String s) {
+        for(int length=s.length();length>=0;length--){
+            for(int start=0;start<=s.length()-length;start++){
+                if(cheak(start,start+length,s)){
+                    return s.substring(start,start+length);
+                }
+            }
         }
-
-        //base case 
-        if(left<0 || right>=s.length()|| s.charAt(left)!=s.charAt(right)){
-            return s.substring(left+1,right);
-        }
-
-        String ans=expand(s,left-1,right+1);
-
-        if(left>=0 && right<s.length()){
-            memo[left][right]=ans;
-        }
-        return ans;
+        return "";
     }
 
-    public String longestPalindrome(String s) {
-        memo=new String[s.length()][s.length()];
-        String longest="";
 
-        for(int i=0;i<s.length();i++){
-            String odd=expand(s,i,i);
-            String even=expand(s,i,i+1);
-            if(odd.length()>longest.length()){
-                longest=odd;
+    public boolean cheak(int i,int j,String s){
+        int left=i;
+        int right=j-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false ;
             }
-            if(even.length()>longest.length()){
-                longest=even;
-            }
+            left++;
+            right--;
         }
-        return longest;
+        return true;
     }
 }
