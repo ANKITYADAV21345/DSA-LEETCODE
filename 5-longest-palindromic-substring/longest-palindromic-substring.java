eccae954277e56778e821a7840f097c3793769fc
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 16:14:12 +0530

    Time: 21 ms (39.94%) | Memory: 46.9 MB (16.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,48 +1,35 @@
-//done by own(tabular)
-
-//table
-//   0 1 2 3 4      
-// 0 T F T F F
-// 1   T F T F
-// 2     T F F
-// 3       T F
-// 4         T
-
-
-
 class Solution {
     public String longestPalindrome(String s) {
-        int n=s.length();
-        boolean[][] dp=new boolean[n][n];
-        String ans="";
-        int maxLen=0;
+        if(s.length()<=1)
+            return s;
 
-        //fill table from botom to top
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j<n;j++){
+        String LPS="";
+        for(int i=1;i<s.length();i++){
+            int low=i;
+            int high=i;
+            while(low>=0 && high<s.length() && s.charAt(low)==s.charAt(high)){
+                low--;
+                high++;
+            }
 
-                //case 1 single char
-                if(i==j){
-                    dp[i][j]=true;
-                }
+            String palindrome=s.substring(low+1,high);
+            if(palindrome.length()>LPS.length()){
+                LPS=palindrome;
+            }
 
-                //two characters  (ADJACENT CHARACTER CHEAK WEATHER SAME OR NOT)
-                else if(j-i==1 && s.charAt(i)==s.charAt(j)){
-                    dp[i][j]=true;
-                }
+            low=i-1;
+            high=i;
+            while(low>=0 && high<s.length() && s.charAt(low)==s.charAt(high)){
+                low--;
+                high++;
+            }
 
-                //case 3 length3 or more 
-                else if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]){
-                    dp[i][j]=true;
-                }
-
-                //TRACK longest
-                if(dp[i][j]&& (j-i+1>maxLen)){
-                    maxLen=j-i+1;
-                    ans=s.substring(i,j+1);  // j+1 iss liye kyoki substring me last index include nahi hota
-                }
+            palindrome=s.substring(low+1,high);
+            if(palindrome.length()>LPS.length()){
+                LPS=palindrome;
             }
         }
-        return ans;
+        return LPS;
+        
     }
 }
