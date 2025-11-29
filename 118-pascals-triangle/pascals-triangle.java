276e2f8a7b1273c01c88c4e7a532fb194bc588e1
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 15:02:43 +0530

    Time: 1 ms (89.93%) | Memory: 43.7 MB (9.55%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,29 +1,36 @@
+// pascal(i, j) = pascal(i-1, j-1) + pascal(i-1, j)
 
-
-//tabulation
 class Solution {
+    Integer[][] memo;
+
     public List<List<Integer>> generate(int numRows) {
-        int[][] dp = new int[numRows][numRows];
+        memo = new Integer[numRows][numRows];
         List<List<Integer>> result = new ArrayList<>();
 
-        // Bottom-Up DP table fill
         for (int i = 0; i < numRows; i++) {
-            dp[i][0] = 1; // first element of each row is 1
-
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-            }
-
-            dp[i][i] = 1; // last element of each row is 1
-
-            // convert dp row to List for result
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
-                row.add(dp[i][j]);
+                row.add(pascal(i, j));
             }
             result.add(row);
         }
 
         return result;
     }
+
+    private int pascal(int i, int j) {
+        // Base cases
+        if (j == 0 || j == i)
+            return 1;
+
+        // Memo check
+        if (memo[i][j] != null)
+            return memo[i][j];
+
+        // Recursive call + memo
+        memo[i][j] = pascal(i - 1, j - 1) + pascal(i - 1, j);
+        return memo[i][j];
+    }
 }
+
+
