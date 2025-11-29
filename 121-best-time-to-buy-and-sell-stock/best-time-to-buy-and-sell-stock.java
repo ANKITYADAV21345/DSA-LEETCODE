cc5e2088de2a61087966fd7b06f4e99abfbe18de
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 15:20:54 +0530

    Time: 103 ms (2.11%) | Memory: 141.9 MB (9.72%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,39 @@
-// dp[i] = max profit possible till day i
+// solve(i, buyDone, minPrice)
 
-//tabular
+
+//recursion +memoisation
 class Solution {
+    Integer[][] memo;
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[] dp = new int[n];
+        memo = new Integer[n][3];
+        return solve(prices, 0, 0);
+    }
 
-        int minPrice = prices[0];
-        dp[0] = 0; // No profit on first day
+    private int solve(int[] prices, int i, int state) {
+        if (i == prices.length) return 0;
+        if (memo[i][state] != null) return memo[i][state];
 
-        for (int i = 1; i < n; i++) {
-            minPrice = Math.min(minPrice, prices[i]); 
-            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
+        int profit = 0;
+
+        if (state == 0) { 
+            // Buy allowed
+            int buy = -prices[i] + solve(prices, i + 1, 1);
+            int skip = solve(prices, i + 1, 0);
+            profit = Math.max(buy, skip);
+
+        } else if (state == 1) { 
+            // Holding stock
+            int sell = prices[i] + solve(prices, i + 1, 2);
+            int hold = solve(prices, i + 1, 1);
+            profit = Math.max(sell, hold);
+
+        } else { 
+            // state == 2 -> already sold, no buy allowed
+            profit = solve(prices, i + 1, 2);
         }
 
-        return dp[n - 1];
+        return memo[i][state] = profit;
     }
 }
-
