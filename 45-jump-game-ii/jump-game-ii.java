20b8483facfae12cf769e24833847a50c648f334
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-11-29 13:42:59 +0530

    Time: 131 ms (5.62%) | Memory: 47.5 MB (6.16%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,25 +1,29 @@
-//apna college notes
-
+//recursion +memoisatrion
 class Solution {
+    int[] memo;
+
     public int jump(int[] nums) {
-        int n=nums.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,-1);
-        dp[n-1]=0;
+        int n = nums.length;
+        memo = new int[n];
+        Arrays.fill(memo, -1);
+        return solve(nums, 0);
+    }
 
-        for(int i=n-2;i>=0;i--){
-            int steps=nums[i];
-            int ans=Integer.MAX_VALUE;
+    private int solve(int[] nums, int i) {
+        int n = nums.length;
+        
+        if (i == n - 1) return 0;
+        if (nums[i] == 0) return (int)1e9; // unreachable
+        
+        if (memo[i] != -1) return memo[i];
 
-            for(int j=i+1;j<=i+steps && j<n;j++){
-                if(dp[j]!=-1){
-                    ans=Math.min(ans,dp[j]+1);
-                }
-            }
-            if(ans!=Integer.MAX_VALUE){
-                dp[i]=ans;
-            }
+        int steps = nums[i];
+        int ans = (int)1e9;
+
+        for (int j = i + 1; j <= i + steps && j < n; j++) {
+            ans = Math.min(ans, 1 + solve(nums, j));
         }
-        return dp[0];
+
+        return memo[i] = ans;
     }
 }
