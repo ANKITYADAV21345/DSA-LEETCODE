708b9936214eddc11ac6b87829c88bcc460d65c0
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-13 01:31:48 +0530

    Time: 79 ms (30.36%) | Memory: 56.4 MB (14.65%) - LeetSync

--- 472-concatenated-words/concatenated-words.java
+++ 472-concatenated-words/concatenated-words.java
@@ -1,8 +1,8 @@
-//optimised code using dp  gpt
 import java.util.*;
 
 class Solution {
     HashSet<String> set;
+    HashMap<String, Boolean> memo;
 
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> res = new ArrayList<>();
@@ -10,33 +10,41 @@ class Solution {
             return res;
 
         set = new HashSet<>(Arrays.asList(words));
+        memo = new HashMap<>();
 
         for (String word : words) {
             if (word.length() == 0) continue;
-            set.remove(word); // avoid using the word itself
+
+            set.remove(word);  // avoid using itself
+
             if (isConcat(word)) {
                 res.add(word);
             }
-            set.add(word); // restore for next iteration
+
+            set.add(word); // add it back
         }
 
         return res;
     }
 
     private boolean isConcat(String word) {
-        int len = word.length();
-        boolean[] dp = new boolean[len + 1];
-        dp[0] = true;
+        if (memo.containsKey(word))
+            return memo.get(word);
 
-        for (int i = 1; i <= len; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && set.contains(word.substring(j, i))) {
-                    dp[i] = true;
-                    break;
-                }
+        // Try splitting the word into prefix + suffix
+        for (int i = 1; i < word.length(); i++) {
+
+            String left = word.substring(0, i);
+            String right = word.substring(i);
+
+            // If prefix exists and suffix is concatenated recursively
+            if (set.contains(left) && (set.contains(right) || isConcat(right))) {
+                memo.put(word, true);
+                return true;
             }
         }
 
-        return dp[len];
+        memo.put(word, false);
+        return false;
     }
 }
