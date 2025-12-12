d5247d49aaa7ea844d5b3886b95609caa851a1d8
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-13 00:07:16 +0530

    Time: 1 ms (71.08%) | Memory: 42.9 MB (17.13%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,42 +1,27 @@
+//tabulation
 import java.util.*;
 
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        HashMap<Integer, Integer> dp = new HashMap<>();
-        return recur(target, nums, new ArrayList<>(), dp);
-    }
+        int[] dp = new int[target + 1];
+        dp[0] = 1;  // base case: one way to make target = 0
 
-    public int recur(int target, int[] nums, ArrayList<Integer> cur, HashMap<Integer, Integer> dp) {
-        // Base cases
-        if (target == 0) {
-            // Uncomment below to print valid combinations
-            // System.out.println(cur);
-            return 1;
-        }
-        if (target < 0) return 0;
-
-        // Memoization check
-        if (dp.containsKey(target)) {
-            return dp.get(target);
-        }
-
-        int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] <= target) {
-                cur.add(nums[i]); // add number to current combination
-                res += recur(target - nums[i], nums, cur, dp); // recurse
-                cur.remove(cur.size() - 1); // backtrack
+        for (int t = 1; t <= target; t++) {
+            for (int num : nums) {
+                if (num <= t) {
+                    dp[t] += dp[t - num];
+                }
             }
         }
 
-        dp.put(target, res); // memoize result
-        return res;
+        return dp[target];
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
         int[] nums = {1, 2, 3};
         int target = 4;
+
         int result = sol.combinationSum4(nums, target);
         System.out.println("Total combinations: " + result);
     }
