22dd38a6a0c0f333710df961210c0b982ecfe10f
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-13 22:20:18 +0530

    Time: 20 ms (22.49%) | Memory: 58 MB (7.30%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,21 +1,28 @@
-//codebix + Chatgpt
 class Solution {
     public int change(int amount, int[] coins) {
-        Integer[][] memo = new Integer[amount + 1][coins.length];
-        return helper(amount, coins, 0, memo);
-    }
 
-    private int helper(int amount, int[] coins, int index, Integer[][] memo) {
-        if (amount == 0) return 1;
-        if (amount < 0 || index == coins.length) return 0;
+        int n = coins.length;
+        int[][] dp = new int[amount + 1][n + 1];
 
-        if (memo[amount][index] != null) return memo[amount][index];
+        // Base case: amount = 0
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 1;
+        }
 
-        // Two choices: include current coin or skip it
-        int include = helper(amount - coins[index], coins, index, memo);
-        int exclude = helper(amount, coins, index + 1, memo);
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 1; j <= n; j++) {
 
-        memo[amount][index] = include + exclude;
-        return memo[amount][index];
+                // Exclude current coin
+                dp[i][j] = dp[i][j - 1];
+
+                // Include current coin
+                if (i - coins[j - 1] >= 0) {
+                    dp[i][j] += dp[i - coins[j - 1]][j];
+                }
+            }
+        }
+
+        return dp[amount][n];
     }
 }
+
