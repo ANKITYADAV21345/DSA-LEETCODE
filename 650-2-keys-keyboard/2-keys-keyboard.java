c3fcdafb99cffdcf9802e98e7e2a2a866aa262d3
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-14 00:08:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (35.67%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,47 +1,33 @@
-//this aproach is giving TLE
-//codebix
-// class Solution {
-//     public int minSteps(int n) {
-//         int current = 1;
-//         int count = 0;
-//         int buffer = 0;
-//         while (current < n) {
-//             int rest = n - current;
-
-//             if (rest % current == 0) {
-//                 buffer = current;
-//                 current = current + buffer;
-//                 count = count + 2;
-//             } else {
-//                 count = count + buffer;
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-// }
-
-
-
-
-// Use Prime Factorization. For each factor of n, the best we can do is "copy" and "paste" that factor times.
-
-// Why this works:
-// If n is divisible by i, then it means you can get to n by copying at n/i and pasting i times.
-
-// This is the minimal step approach because each factor corresponds to a "Copy All" + multiple "Pastes".
+//RECURSION + MEMOISATION
 class Solution {
+
+    int[] memo;
+
     public int minSteps(int n) {
-        int res = 0;
-        for (int i = 2; i <= n; i++) {
-            while (n % i == 0) {
-                res += i;
-                n /= i;
+        memo = new int[n + 1];
+        return solve(n);
+    }
+
+    private int solve(int n) {
+
+        if (n == 1) return 0;
+
+        if (memo[n] != 0)
+            return memo[n];
+
+        int ans = n; // worst case: copy 1, paste (n-1) times
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                ans = Math.min(ans, solve(n / i) + i);
             }
         }
-        return res;
+
+        memo[n] = ans;
+        return ans;
     }
 }
+
 
 
 
