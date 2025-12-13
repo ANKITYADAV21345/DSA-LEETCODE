9b7a2d7b76e3c64b3bfc9247056ac8f109933a5f
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-14 00:46:05 +0530

    Time: 1 ms (28.86%) | Memory: 44 MB (5.04%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,32 +1,42 @@
 class Solution {
+
+    String s;
+    Boolean[][] memo;
+
     public boolean checkValidString(String s) {
+        this.s = s;
+        memo = new Boolean[s.length()][s.length() + 1];
+        return dfs(0, 0);
+    }
 
-        int n = s.length();
-        boolean[][] dp = new boolean[n + 1][n + 1];
+    private boolean dfs(int index, int open) {
 
-        dp[0][0] = true;
+        // invalid
+        if (open < 0) return false;
 
-        for (int i = 0; i < n; i++) {
-            for (int open = 0; open <= n; open++) {
-
-                if (!dp[i][open]) continue;
-
-                char c = s.charAt(i);
-
-                if (c == '(') {
-                    dp[i + 1][open + 1] = true;
-                } else if (c == ')') {
-                    if (open > 0)
-                        dp[i + 1][open - 1] = true;
-                } else { // '*'
-                    dp[i + 1][open] = true;        // empty
-                    dp[i + 1][open + 1] = true;    // '('
-                    if (open > 0)
-                        dp[i + 1][open - 1] = true; // ')'
-                }
-            }
+        // end of string
+        if (index == s.length()) {
+            return open == 0;
         }
 
-        return dp[n][0];
+        if (memo[index][open] != null)
+            return memo[index][open];
+
+        char c = s.charAt(index);
+        boolean valid;
+
+        if (c == '(') {
+            valid = dfs(index + 1, open + 1);
+        } else if (c == ')') {
+            valid = dfs(index + 1, open - 1);
+        } else { // '*'
+            valid =
+                dfs(index + 1, open) ||       // empty
+                dfs(index + 1, open + 1) ||   // '('
+                dfs(index + 1, open - 1);     // ')'
+        }
+
+        memo[index][open] = valid;
+        return valid;
     }
 }
