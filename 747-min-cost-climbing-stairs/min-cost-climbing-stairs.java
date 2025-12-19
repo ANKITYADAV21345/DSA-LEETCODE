0f2ab8be5c82ba4adb8bbc1c221a52c2daaacfdf
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-20 01:22:09 +0530

    Time: 1 ms (38.38%) | Memory: 45 MB (14.00%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,15 +1,32 @@
-//nikhil lohia
 class Solution {
+
+    int[] memo;
+
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] minCost = new int[n + 1];
+        memo = new int[n + 1];
 
-        for (int i = 2; i <= n; i++) {
-            minCost[i] = Math.min(
-                    (cost[i - 1] + minCost[i - 1]),
-                    (cost[i - 2] + minCost[i - 2]));
+        // -1 means not computed
+        for (int i = 0; i <= n; i++) {
+            memo[i] = -1;
         }
 
-        return minCost[n];
+        return solve(cost, n);
+    }
+
+    private int solve(int[] cost, int i) {
+
+        // base cases
+        if (i == 0 || i == 1)
+            return 0;
+
+        if (memo[i] != -1)
+            return memo[i];
+
+        int oneStep = cost[i - 1] + solve(cost, i - 1);
+        int twoStep = cost[i - 2] + solve(cost, i - 2);
+
+        memo[i] = Math.min(oneStep, twoStep);
+        return memo[i];
     }
 }
