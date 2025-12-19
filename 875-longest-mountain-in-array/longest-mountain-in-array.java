4c931866eb67c3f0e12440eccb8306004b8276bc
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-20 01:35:59 +0530

    Time: 3 ms (78.25%) | Memory: 47.2 MB (54.55%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,35 +1,45 @@
-//done by own 
+// inc[i] = length of increasing sequence ending at i
+
+// dec[i] = length of decreasing sequence starting at i
+
+// Mountain tab banegi jab:
+
+// inc[i] > 1 && dec[i] > 1
+// length = inc[i] + dec[i] - 1
+
+
+
 class Solution {
     public int longestMountain(int[] arr) {
         int n = arr.length;
         if (n < 3) return 0;
 
-        int ans = 0;
+        int[] inc = new int[n];
+        int[] dec = new int[n];
 
-        for (int i = 1; i < n - 1; i++) {
-
-            // peak condition
-            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-                int left = i - 1;
-                int right = i + 1;
-                int count = 1;
-
-                // count left increasing
-                while (left >= 0 && arr[left] < arr[left + 1]) {
-                    count++;
-                    left--;
-                }
-
-                // count right decreasing
-                while (right < n && arr[right] < arr[right - 1]) {
-                    count++;
-                    right++;
-                }
-
-                ans = Math.max(ans, count);
+        // increasing sequence
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                inc[i] = inc[i - 1] + 1;
             }
         }
+
+        // decreasing sequence
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                dec[i] = dec[i + 1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (inc[i] > 0 && dec[i] > 0) {
+                ans = Math.max(ans, inc[i] + dec[i] + 1);
+            }
+        }
+
         return ans;
     }
 }
+
 
