83dca896df1b55fab2ed7e52f690199e528e7b09
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-20 01:44:35 +0530

    Time: 247 ms (13.10%) | Memory: 109 MB (35.12%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,32 +1,42 @@
+//memoisation
 class Solution {
+
+    int[][] memo;
+    Map<Integer, Integer> map;
+
     public int lenLongestFibSubseq(int[] arr) {
-        if(arr==null || arr.length==0){
-            return 0;
+        int n = arr.length;
+        memo = new int[n][n];
+        map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], i);
         }
-        int len=arr.length;
-        int [][] dp=new int [len][len];
-        Map<Integer,Integer> m=new HashMap<>();
-        for(int i=0;i<len;i++){
-            m.put(arr[i],i);
-            for(int j=i+1;j<len;j++){
-                dp[i][j]=2;
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                ans = Math.max(ans, solve(arr, i, j));
             }
         }
-        for(int i=0;i<len;i++){
-            for(int j=i+1;j<len;j++){
-                int k=arr[j]+arr[i];
-                if(m.containsKey(k)){
-                    int index=m.get(k);
-                    dp[j][index]=dp[i][j]+1;
-                }
-            }
+
+        return ans < 3 ? 0 : ans;
+    }
+
+    private int solve(int[] arr, int i, int j) {
+
+        if (memo[i][j] != 0)
+            return memo[i][j];
+
+        int next = arr[i] + arr[j];
+
+        if (!map.containsKey(next)) {
+            memo[i][j] = 2;
+            return 2;
         }
-        int res=0;
-        for(int i=0;i<len;i++){
-            for(int j=i+1;j<len;j++){
-                res=Math.max(res,dp[i][j]);
-            }
-        }
-        return res==2?0: res;
+
+        int k = map.get(next);
+        memo[i][j] = 1 + solve(arr, j, k);
+        return memo[i][j];
     }
 }
