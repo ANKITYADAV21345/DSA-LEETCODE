fce278e344661ec8bb19b14763b021a1b2c7ac12
ANKITYADAV21345 <ankityadav21345@gmail.com>
2025-12-26 21:37:52 +0530

    Time: 3 ms (98.72%) | Memory: 47.3 MB (38.39%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int counta=0;
+        int countb=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a'){
+                counta++;
+            }
+            else{
+                countb++;
+            }
+        }
+        int result=Math.abs(counta-countb);
+        return result;
+    }
+}
