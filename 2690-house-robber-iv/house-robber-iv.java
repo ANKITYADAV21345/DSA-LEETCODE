528847a2e6a20e6470f1d8efcb8e708a86784a10
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-01-17 11:20:29 +0530

    Time: 18 ms (71.29%) | Memory: 76.1 MB (56.45%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,14 +1,18 @@
+//coding with vinod
+//binary search in a range
 class Solution {
     public int minCapability(int[] nums, int k) {
         int left=Integer.MAX_VALUE;
         int res=0;
         int right=Integer.MIN_VALUE;
+
         for(int i:nums){
             left=Math.min(left,i);
             right=Math.max(right,i);
         }
+
         while(left<=right){
-            int mid=left+((right-left)/2);
+            int mid=left+(right-left)/2;
             if(selectk(nums,k,mid)){
                 res=mid;
                 right=mid-1;
@@ -16,37 +20,19 @@ class Solution {
                 left=mid+1;
             }
         }
-        return res; 
+        return res;
     }
-    public boolean selectk(int[] nums,int k,int mid){
+
+    public boolean selectk(int [] nums,int k,int mid){
         int count=0;
         int i=0;
         while(i<nums.length){
             if(nums[i]<=mid){
-                count++;
-                i++;
-            }
+            count++;
             i++;
+        }
+        i++;
         }
         return count>=k;
     }
 }
-
-
-// int n=nums.length;
-        // ArrayList<Integer> ans=new ArrayList<>();
-        // for(int i=0;i<n-2;i++){
-        //     int j=i+2;
-        //     while(j<n){
-        //         if(j<n-1){
-        //             ans.add(Math.max(nums[i],nums[j]));
-        //         }else{
-        //             ans.add(nums[i]);
-        //             }
-        //         }   
-        //     }
-        // int minnum=Integer.MAX_VALUE;
-        // for(int i:ans){
-        //     minnum=Math.min(i,minnum);
-        // }
-        // return minnum;
