ef88fea3f9b73ceba227c8f399af7aad573c4b13
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-01-26 15:03:17 +0530

    Time: 65 ms (67.64%) | Memory: 51.6 MB (61.97%) - LeetSync

--- 472-concatenated-words/concatenated-words.java
+++ 472-concatenated-words/concatenated-words.java
@@ -1,50 +1,38 @@
 import java.util.*;
-
 class Solution {
     HashSet<String> set;
-    HashMap<String, Boolean> memo;
-
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> res = new ArrayList<>();
-        if (words == null || words.length == 0)
-            return res;
-
-        set = new HashSet<>(Arrays.asList(words));
-        memo = new HashMap<>();
-
-        for (String word : words) {
-            if (word.length() == 0) continue;
-
-            set.remove(word);  // avoid using itself
-
-            if (isConcat(word)) {
-                res.add(word);
-            }
-
-            set.add(word); // add it back
+        List<String> result=new ArrayList<>();
+        if(words==null || words.length==0){
+            return result;
         }
-
-        return res;
+        set=new HashSet<>(Arrays.asList(words));
+        for(String word : words){
+            if(word.length()==0){
+                continue;
+            }
+            set.remove(word);
+            if(isconcat(word)){
+                result.add(word);
+            }
+            set.add(word);
+        }
+        return result;
     }
 
-    private boolean isConcat(String word) {
-        if (memo.containsKey(word))
-            return memo.get(word);
+    public boolean isconcat(String words){
+        int len=words.length();
+        boolean dp[]=new boolean[len+1];
+        dp[0]=true;
+        for(int i=1;i<=len;i++){
+            for(int j=0;j<i;j++){
+                if (dp[j] && set.contains(words.substring(j, i))) {
 
-        // Try splitting the word into prefix + suffix
-        for (int i = 1; i < word.length(); i++) {
-
-            String left = word.substring(0, i);
-            String right = word.substring(i);
-
-            // If prefix exists and suffix is concatenated recursively
-            if (set.contains(left) && (set.contains(right) || isConcat(right))) {
-                memo.put(word, true);
-                return true;
+                    dp[i]=true;
+                    break;
+                }
             }
         }
-
-        memo.put(word, false);
-        return false;
+        return dp[len];
     }
 }
