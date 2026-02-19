71dbf32af7b56d5ecc9fd5e6a4417f8e457b1bc5
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-02-19 23:10:32 +0530

    Time: 50 ms (13.14%) | Memory: 94.8 MB (22.63%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.java
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -1,29 +1,27 @@
-//Learn overflow
-import java.util.*;
-
+// coders camp(isse samajha hai ache se baki jitne bhi submited hai confusin g hai but ye sahi hai )
 class Solution {
     public int maxResult(int[] nums, int k) {
-        int n = nums.length;
-        Deque<Integer> dq = new ArrayDeque<>();
-        dq.offer(0);
+        //sort on the bases of score
+        Queue<int[]> pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
 
-        for (int i = 1; i < n; i++) {
-            // nums[i] gets the max score from the last k elements
-            nums[i] += nums[dq.peekFirst()];
+        //store kar raha hai score aur index initially starting index
+        pq.offer(new int[]{nums[0],0});
 
-            // Maintain deque in decreasing order of nums[]
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
-                dq.pollLast();
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            //if we no longer can reach to a position then we can pop from our queue
+            while(pq.peek()[1]<i-k){
+                pq.poll();
             }
 
-            dq.offerLast(i);
-
-            // Remove elements out of the k-window
-            if (i - dq.peekFirst() >= k) {
-                dq.pollFirst();
-            }
+            //main logic
+            //cur top element ko store rakhega 
+            int [] cur=pq.peek();
+            //calculate karege score 
+            max=cur[0]+nums[i];
+            //aur fir add kar dege pq
+            pq.offer(new int[]{cur[0]+nums[i],i});
         }
-
-        return nums[n - 1];
+        return max;
     }
 }
