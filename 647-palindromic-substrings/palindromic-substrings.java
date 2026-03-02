2665eb23cf8462821fdcfec71eff44ec296247be
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-03-02 12:49:32 +0530

    Time: 30 ms (21.94%) | Memory: 53.3 MB (6.41%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,31 +1,53 @@
-//tabulation conversion from memo
 class Solution {
+
+    int n;
+    String s;
+    int[][] memo;
 
     public int countSubstrings(String s) {
 
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
+        this.s = s;
+        n = s.length();
+        memo = new int[n][n];
+
+        // initialize memo with -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
         int count = 0;
 
-        for(int i = n - 1; i >= 0; i--) {
-
-            for(int j = i; j < n; j++) {
-
-                if(s.charAt(i) == s.charAt(j)) {
-
-                    if(j - i <= 1) {
-                        dp[i][j] = true;   // length 1 or 2
-                    }
-                    else {
-                        dp[i][j] = dp[i+1][j-1];
-                    }
-
-                    if(dp[i][j])
-                        count++;
+        // try all substrings
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(i, j)) {
+                    count++;
                 }
             }
         }
 
         return count;
+    }
+
+    public boolean isPalindrome(int i, int j) {
+
+        // base case
+        if (i >= j) return true;
+
+        // if already computed
+        if (memo[i][j] != -1)
+            return memo[i][j] == 1;
+
+        boolean result = false;
+
+        if (s.charAt(i) == s.charAt(j)) {
+            result = isPalindrome(i + 1, j - 1);
+        }
+
+        memo[i][j] = result ? 1 : 0;
+
+        return result;
     }
 }
