3fa5c5bd2275eb6a8227c75cef202b4c358a3791
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-03-02 12:32:35 +0530

    Time: 20 ms (26.79%) | Memory: 46 MB (32.21%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,31 @@
+//tabulation conversion from memo
+class Solution {
+
+    public int countSubstrings(String s) {
+
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            for(int j = i; j < n; j++) {
+
+                if(s.charAt(i) == s.charAt(j)) {
+
+                    if(j - i <= 1) {
+                        dp[i][j] = true;   // length 1 or 2
+                    }
+                    else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+
+                    if(dp[i][j])
+                        count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
