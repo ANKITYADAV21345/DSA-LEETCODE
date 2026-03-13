e0b2c0fdac69be9b992f7aee2e419353006a4b2c
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-03-13 11:42:09 +0530

    Time: 1 ms (99.85%) | Memory: 47.1 MB (94.43%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,24 +1,28 @@
+//apna college article bruteforce
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashSet<Integer> set=new HashSet<>();
-        int n=grid.length;
-        int sq=n*n;
+        int n = grid.length;
+        int N = n * n;
+        int[] freq = new int[N + 1]; // Frequency array
+        // Initialise repeated and missing with -1
+        int repeated = -1, missing = -1;
 
-        int cursum=0;
-        int ans[]=new int[2];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(set.contains(grid[i][j])){
-                    ans[0]=grid[i][j];
-                }
-                else{
-                    set.add(grid[i][j]);
-                    cursum=cursum+grid[i][j];
-                }
+        // Count occurrences of each number in the grid
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                freq[grid[i][j]]++;
             }
         }
-        int totalsum=sq*(sq+1)/2;
-        ans[1]=totalsum-cursum;
-        return ans;
+
+        // Find the repeated and missing numbers
+        for (int i = 1; i <= N; i++) {
+            if (freq[i] == 2) {
+                repeated = i;
+            } else if (freq[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[] { repeated, missing };
     }
 }
