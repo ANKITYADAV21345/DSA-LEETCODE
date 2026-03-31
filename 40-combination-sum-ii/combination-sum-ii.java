532c6ca68b8c6dbc413b1d92597eed73aef66344
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-01 01:46:01 +0530

    Time: 5 ms (99.54%) | Memory: 45 MB (84.33%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,32 +1,45 @@
-//by code shash recursion and backtracking
-
-import java.util.*;
-
+//apna college(aditorial optimised) isme bass maine backtrack me i+1 kar diya i ki jagah and duplicates hata diye nums[i]==nums[i-1] vali condition se 
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        ArrayList<Integer> cur = new ArrayList<>();
-        Arrays.sort(candidates); // sort the candidates to handle duplicates
-        recur(candidates, target, 0, cur, res);
-        return res;
-    }
+   public List<List<Integer>> combinationSum2(int[] nums, int target) {
+       Arrays.sort(nums); // Enables pruning
+       List<List<Integer>> result = new ArrayList<>();
+       List<Integer> curr = new ArrayList<>();
+       backtrack(nums, target, 0, curr, result);
+       return result;
+   }
 
-    public void recur(int[] candidates, int target, int ind, ArrayList<Integer> cur, List<List<Integer>> res) {
-        if (target == 0) {
-            res.add(new ArrayList<>(cur)); // base case
-            return;
-        }
 
-        for (int i = ind; i < candidates.length; i++) {
-            if (i > ind && candidates[i] == candidates[i - 1]) {
+   private void backtrack(int[] nums, int target, int start, List<Integer> curr, List<List<Integer>> result) {
+       if (target == 0) {
+           result.add(new ArrayList<>(curr)); // copy the current combination
+           return;
+       }
+
+
+       for (int i = start; i < nums.length; i++) {
+
+            if (i > start && nums[i] == nums[i - 1]) {
                 continue; // skip duplicates
             }
-            if (candidates[i] > target) {
-                break; // no need to continue if candidate exceeds target
-            }
-            cur.add(candidates[i]);
-            recur(candidates, target - candidates[i], i + 1, cur, res);
-            cur.remove(cur.size() - 1); // backtrack
-        }
-    }
+
+
+           if (nums[i] > target) break; // Prune the path
+
+
+           curr.add(nums[i]);
+           backtrack(nums, target - nums[i], i+1, curr, result); 
+           curr.remove(curr.size() - 1); // Backtrack
+       }
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
