2669a286fe843a38b51fdb1cd8031b5fd95a50ed
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-08 00:33:23 +0530

    Time: 25 ms (26.99%) | Memory: 54.2 MB (13.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,41 +1,51 @@
-//codebix
+// apna college
+//recursion+memo
 class Solution {
 
-    int[][] memo;
-
     public int longestCommonSubsequence(String text1, String text2) {
-        memo = new int[text1.length()][text2.length()];
 
-        // initialize memo with -1
-        for (int i = 0; i < text1.length(); i++) {
-            for (int j = 0; j < text2.length(); j++) {
+        int n = text1.length();
+        int m = text2.length();
+
+        // Step 1: memo array
+        // memo[i][j] = LCS length of first i chars of text1 and first j chars of text2
+        int[][] memo = new int[n + 1][m + 1];
+
+        // initialize with -1 (means not computed)
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= m; j++){
                 memo[i][j] = -1;
             }
         }
 
-        return lcs(text1, text2, 0, 0);
+        return LCS(text1, text2, n, m, memo);
     }
 
-    private int lcs(String s1, String s2, int i, int j) {
+    public int LCS(String text1, String text2, int n, int m, int[][] memo){
 
-        // base case
-        if (i == s1.length() || j == s2.length())
+        // Step 2: Base case
+        // agar koi string empty hai → LCS = 0
+        if(n == 0 || m == 0){
             return 0;
-
-        // memo check
-        if (memo[i][j] != -1)
-            return memo[i][j];
-
-        // recursive logic
-        if (s1.charAt(i) == s2.charAt(j)) {
-            memo[i][j] = 1 + lcs(s1, s2, i + 1, j + 1);
-        } else {
-            memo[i][j] = Math.max(
-                    lcs(s1, s2, i + 1, j),
-                    lcs(s1, s2, i, j + 1)
-            );
         }
 
-        return memo[i][j];
+        // Step 3: check memo (already computed)
+        if(memo[n][m] != -1){
+            return memo[n][m];
+        }
+
+        // Step 4: if characters match
+        if(text1.charAt(n - 1) == text2.charAt(m - 1)){
+            memo[n][m] = 1 + LCS(text1, text2, n - 1, m - 1, memo);
+        } 
+        else{
+            // Step 5: if not match → take max of two choices
+            int ans1 = LCS(text1, text2, n - 1, m, memo);
+            int ans2 = LCS(text1, text2, n, m - 1, memo);
+
+            memo[n][m] = Math.max(ans1, ans2);
+        }
+
+        return memo[n][m];
     }
 }
