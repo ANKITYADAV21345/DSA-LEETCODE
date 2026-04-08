b5340c4d2b0b2348c770533c045d4287a685a9de
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-09 00:58:23 +0530

    Time: 5 ms (15.76%) | Memory: 43.8 MB (8.34%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,58 +1,30 @@
-
-//  aproach 1 
-
-// class Solution {
-//     public boolean isValid(String s) {
-//     Stack<Character> stack = new Stack<>();
-
-//     for (char c : s.toCharArray()) {
-
-//         if (c == '(')
-//             stack.push(')');
-//         else if (c == '{')
-//             stack.push('}');
-//         else if (c == '[')
-//             stack.push(']');
-
-//         else if (stack.isEmpty() || stack.pop() != c)
-//             return false;
-//     }
-//     return stack.isEmpty();
-//     }
-// }
+//apna college
 
 
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
 
-// aproach 2
-class Solution{
-    
-    public char getVal(char ch){
-        switch(ch){
-            case ']' : return '[';
-            case '}' : return '{';
-            case ')' : return '(';
-            default  : return ' '; 
-        }
-    }
+        for (int i = 0; i < s.length(); i++) {
 
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
 
-    public boolean isValid(String s){
-        String opening="({[";
-        String closing="]})";
-
-        Stack<Character> st=new Stack<>();
-        
-        for(char ch : s.toCharArray()){
-            if(opening.indexOf(ch)!=-1)
-            st.push(ch);
-            else{
-                if(st.size()==0) return false;
-                char tmp=st.pop();
-                if(getVal(ch)!=tmp)
-                return false;
+                if ((stack.peek() == '(' && s.charAt(i) == ')') ||
+                    (stack.peek() == '{' && s.charAt(i) == '}') ||
+                    (stack.peek() == '[' && s.charAt(i) == ']')) {
+                    
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
-
         }
-        return st.size()==0;
+
+        return stack.isEmpty();
     }
 }
