ad48d349ab6dd28cb242b65feb09a67141ac16be
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-10 11:06:45 +0530

    Time: 9 ms (34.20%) | Memory: 46.5 MB (10.54%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,14 +1,32 @@
-//shash code
+//mera logic hai ki queue me dal do and har bar age vale ko last me dal do jab tak k>0 hai
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
-        for (int i = 0; i < tickets.length; i++) {
-            if (i <= k) {
-                time = time + Math.min(tickets[i], tickets[k]);
-            } else {
-                time += Math.min(tickets[i], tickets[k] - 1);
+        Queue<int[]> queue=new LinkedList<>();
+        
+        //store larege pair of tickes and index
+        for(int i=0;i<tickets.length;i++){
+            queue.add(new int[]{tickets[i],i});
+        }
+
+        int time=0;
+        while(!queue.isEmpty()){
+            int [] front=queue.poll();
+            //jab ham eek ticket kharid dege 
+            front[0]--;
+            time++;
+
+            //agar ye banda k hai aur tickent 0 ho gaya to matlab ans mill jayega
+            if(front[1]==k && front[0]==0){
+                return time;
             }
+
+            //agar tickets abhi baki hai to last me bhejo
+            if(front[0]>0){
+                queue.add(front);
+            }
+
         }
         return time;
+
     }
 }
