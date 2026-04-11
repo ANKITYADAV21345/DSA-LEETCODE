3b1513862e44a324d6b70339325c923eec586de5
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-12 01:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (22.28%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,49 +15,30 @@
  */
 
 
-
-// Root ke left aur right subtree ko mirror check karna hai.
-
-// Mirror ka matlab:
-
-// Left ka left == Right ka right
-
-// Left ka right == Right ka left
-
-
-// Agar values match nahi hui → false
-
-// Agar structure mismatch (null mismatch) → false
-
-// Agar sab match hua → true
-
-
-//done by self(recursion)
+ //apna college notes
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        
-        //base case
         if(root==null){
             return true;
         }
-
         return isMirror(root.left,root.right);
     }
-
 
     public boolean isMirror(TreeNode left,TreeNode right){
         if(left==null && right==null){
             return true;
         }
 
+
         if(left==null || right==null){
             return false;
         }
-
         if(left.val!=right.val){
             return false;
         }
 
-        return isMirror(left.left,right.right)  && isMirror(left.right,right.left);
+
+        //imp cheak condition
+        return isMirror(left.left,right.right) && isMirror(left.right,right.left);
     }
 }
