8b687e837f034c4faf5d8dc21adec6dc65da6b85
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-11 17:47:48 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (74.26%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -9,36 +9,76 @@
  * }
  */
 
- //codestorywithmik
- //recursive
+
+
+
+//codestorywithmic
+//github code
 class Solution {
+    // Helper method to calculate GCD of two numbers
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        if(head==null || head.next==null){
+        // If the list is empty or has only one element, return the head as is
+        if (head == null || head.next == null) {
             return head;
         }
 
-        ListNode temp=insertGreatestCommonDivisors(head.next);
-        ListNode curNode=head;
-        ListNode nextNode=head.next;
+        ListNode currNode = head;
+        ListNode nextNode = head.next;
 
-        //gcd ki value nikal rahe hai dono node se 
-        int a = Math.abs(curNode.val);
-        int b = Math.abs(nextNode.val);
+        while (nextNode != null) {
+            // Create a new node with the GCD of current and next node values
+            ListNode temp = new ListNode(gcd(currNode.val, nextNode.val));
+            
+            // Insert the new node between currNode and nextNode
+            currNode.next = temp;
+            temp.next = nextNode;
 
-        // Euclidean algorithm
-        while (b != 0) {
-            int temp1 = b;
-            b = a % b;
-            a = temp1;
+            // Move to the next pair of nodes
+            currNode = nextNode;
+            nextNode = nextNode.next;
         }
 
-        //node bana rahe hai gcd ki
-        ListNode gcdNode=new ListNode(a);
-
-        gcdNode.next=temp;
-        head.next=gcdNode;
-
         return head;
-
     }
 }
+
+
+
+//Approach-2 (Recursion Story)
+//T.C : O(n)
+//S.C : System stack space due to recursion O(n)
+// class Solution {
+//     // Helper method to calculate GCD of two numbers
+//     private int gcd(int a, int b) {
+//         if (b == 0) {
+//             return a;
+//         }
+//         return gcd(b, a % b);
+//     }
+
+//     public ListNode insertGreatestCommonDivisors(ListNode head) {
+//         // Base case: if the list is empty or has only one element, return the head as is
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+
+//         // Insert the GCD node recursively
+//         ListNode temp = insertGreatestCommonDivisors(head.next);
+
+//         // Create a new node with the GCD of current and next node values
+//         ListNode gcdNode = new ListNode(gcd(head.val, head.next.val));
+        
+//         // Insert the new node between current node and next node
+//         gcdNode.next = temp;
+//         head.next = gcdNode;
+
+//         return head;
+//     }
+// }
