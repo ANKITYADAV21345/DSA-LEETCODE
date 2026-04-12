b61f3ccf365242752d28183bebedaeda6deb2b94
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-12 15:00:45 +0530

    Time: 2 ms (46.23%) | Memory: 45.5 MB (5.61%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -15,39 +15,55 @@
  */
 
 
- //done by own (recursion)
+//apna college
+
 class Solution {
 
-    int maxWidth=0;
-    Map<Integer,Long> leftmost=new HashMap<>();
-    public int widthOfBinaryTree(TreeNode root) {
-        dfs(root,0,1L);
-        return maxWidth;
+    class Pair {
+        TreeNode first;
+        long second;
+
+        Pair(TreeNode first, long second) {
+            this.first = first;
+            this.second = second;
+        }
     }
 
-    public void dfs(TreeNode root,int level,long index){
+    public int widthOfBinaryTree(TreeNode root) {
 
-        //base case
-        if(root==null){
-            return;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(root, 0));
+
+        int maxWidth = 0;
+
+        while (queue.size() > 0) {
+
+            int currLevelSize = queue.size();
+
+            long stIdx = queue.peek().second;
+
+            long endIdx = 0;
+            for (Pair p : queue) {
+                endIdx = p.second;
+            }
+
+            maxWidth = Math.max(maxWidth, (int)(endIdx - stIdx + 1));
+
+            for (int i = 0; i < currLevelSize; i++) {
+
+                Pair curr = queue.peek();
+                queue.remove();
+
+                if (curr.first.left != null) {
+                    queue.add(new Pair(curr.first.left, curr.second * 2 + 1));
+                }
+
+                if (curr.first.right != null) {
+                    queue.add(new Pair(curr.first.right, curr.second * 2 + 2));
+                }
+            }
         }
 
-
-        //logic yaha se niche tak
-
-        // store leftmost index of this level
-        if(!leftmost.containsKey(level)){
-            leftmost.put(level,index);
-        }
-
-
-        // width = current_index - first_index_level + 1
-        long currentWidth=index-leftmost.get(level)+1;
-        maxWidth=Math.max(maxWidth,(int) currentWidth);
-
-        
-        // move to left and right children with updated indices
-        dfs(root.left,level+1,2*index);
-        dfs(root.right,level+1,2*index+1);
+        return maxWidth;
     }
 }
