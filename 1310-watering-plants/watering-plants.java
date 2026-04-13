42ce7dd1cf12e515742bfb56bfb9626207717eb6
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-13 12:08:06 +0530

    Time: 1 ms (34.28%) | Memory: 44 MB (88.31%) - LeetSync

--- 1310-watering-plants/watering-plants.java
+++ 1310-watering-plants/watering-plants.java
@@ -1,26 +1,21 @@
+//algorithmic iq
+//watering plant 2 se logiical variable alice jo pani bhar raha hai vaha se liya hai
 class Solution {
     public int wateringPlants(int[] plants, int capacity) {
-        int steps=0;
-        int cap=capacity;
+         int steps=0;
+         int alicewater=capacity;
 
-        for(int i=0;i<plants.length;i++){
-            if(plants[i]<=cap){
-                //walking a step to water the plant
+         for(int i=0;i<plants.length;i++){
+            if(plants[i]<=alicewater){
                 steps++;
-                //reducing jar capacity after watering the plant
-                cap=cap-plants[i];
-
+                alicewater=alicewater-plants[i];
             }
             else{
-                //walking to river to re-fill the jar and coming back to the plant
-                //this takes (2*i)+1 steps
-                cap= capacity;
-
-                //reducing jar capacity after watering the plant 
-                cap=cap-plants[i];
+                alicewater=capacity;
+                alicewater=alicewater-plants[i];
                 steps=steps+(2*i)+1;
             }
-        }
-        return steps;
+         }
+         return steps;
     }
 }
