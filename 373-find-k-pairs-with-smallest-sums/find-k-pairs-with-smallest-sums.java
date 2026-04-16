9244cbd7764a45c0eb9316bbcbd6e8927d0b09a4
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-17 01:08:34 +0530

    Time: 88 ms (8.56%) | Memory: 124 MB (26.28%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -3,46 +3,53 @@ import java.util.*;
 
 public class Solution {
 
-    // Approach-2 (Improved Brute Force)
-    // TIME COMPLEXITY < O(m * n * log(k)) - ACCEPTED
+    // Approach-3 (Slight Better approach) - O(k log k)
 
     public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-
-        // Max Heap (same as C++ priority_queue default)
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> b[0] - a[0]   // max heap based on sum
-        );
 
         int m = nums1.length;
         int n = nums2.length;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        // Min Heap (based on sum)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> a[0] - b[0]
+        );
 
-                int sum = nums1[i] + nums2[j];
+        // visited set to avoid duplicates
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0");
 
-                if (pq.size() < k) {
-                    pq.offer(new int[]{sum, i, j});
-                } 
-                else if (pq.peek()[0] > sum) {
-                    pq.poll();
-                    pq.offer(new int[]{sum, i, j});
-                } 
-                else {
-                    break; // 🔥 important optimization
-                }
-            }
-        }
+        int sum = nums1[0] + nums2[0];
+        pq.offer(new int[]{sum, 0, 0});
 
         List<List<Integer>> result = new ArrayList<>();
 
-        while (!pq.isEmpty()) {
+        while (k-- > 0 && !pq.isEmpty()) {
+
             int[] temp = pq.poll();
 
             int i = temp[1];
             int j = temp[2];
 
             result.add(Arrays.asList(nums1[i], nums2[j]));
+
+            // Push (i, j+1)
+            if (j + 1 < n) {
+                String key = i + "," + (j + 1);
+                if (!visited.contains(key)) {
+                    pq.offer(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
+                    visited.add(key);
+                }
+            }
+
+            // Push (i+1, j)
+            if (i + 1 < m) {
+                String key = (i + 1) + "," + j;
+                if (!visited.contains(key)) {
+                    pq.offer(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
+                    visited.add(key);
+                }
+            }
         }
 
         return result;
