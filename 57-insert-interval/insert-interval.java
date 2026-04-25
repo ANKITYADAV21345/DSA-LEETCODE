dfe2c3a577aaa6eb92c5b4b72de2ac88d778a9e4
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-04-25 23:58:49 +0530

    Time: 8 ms (3.70%) | Memory: 47.2 MB (37.95%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,27 +1,62 @@
-//nikhil lohia
-class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        int[][] result = new int[intervals.length + 1][2];
-        int i = 0, j = 0;
+//codestorywithmik
+//bruteforce
+import java.util.*;
 
-        // Add all intervals that end before the new interval starts
-        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-            result[j++] = intervals[i++];
+public class Solution {
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> list = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            list.add(interval);
         }
 
-        // Merge overlapping intervals
-        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i++][1]);
+        int i = 0;
+
+        // O(n^2) because insert/remove in ArrayList can shift elements
+        while (i < list.size()) { // O(n)
+
+            // Current interval completely before newInterval
+            if (list.get(i)[1] < newInterval[0]) {
+                i++;
+            }
+
+            // Current interval completely after newInterval
+            else if (list.get(i)[0] > newInterval[1]) {
+                list.add(i, newInterval); // insert at position i
+                return list.toArray(new int[list.size()][]);
+            }
+
+            // Overlapping intervals
+            else {
+                // Merge and remove current interval
+                newInterval[0] = Math.min(newInterval[0], list.get(i)[0]);
+                newInterval[1] = Math.max(newInterval[1], list.get(i)[1]);
+
+                list.remove(i); // erase merged interval
+            }
         }
 
-        result[j++] = newInterval;
+        // If newInterval goes at the end
+        list.add(newInterval);
 
-        // Add remaining intervals
-        while (i < intervals.length) {
-            result[j++] = intervals[i++];
+        return list.toArray(new int[list.size()][]);
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {
+                {1, 3},
+                {6, 9}
+        };
+
+        int[] newInterval = {2, 5};
+
+        int[][] result = insert(intervals, newInterval);
+
+        for (int[] interval : result) {
+            System.out.println(interval[0] + " " + interval[1]);
         }
-
-        return java.util.Arrays.copyOf(result, j);
     }
 }
