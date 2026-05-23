d0951ad61add450895acf68c628d4bec44d9fa07
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-05-23 23:21:10 +0530

    Time: 1 ms (99.60%) | Memory: 46.3 MB (81.60%) - LeetSync

---
+++ 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumSwaps(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count++;
+            }
+
+        }
+
+        for(int i=nums.length-count;i<nums.length;i++){
+            if(nums[i]==0){
+                count--;
+            }
+        }
+        return count;
+    }
+}
