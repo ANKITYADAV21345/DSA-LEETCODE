85e1ca781a0870549259fdd7e91365465883ef16
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-06-27 16:29:10 +0530

    Time: 4 ms (86.81%) | Memory: 45.2 MB (39.77%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,71 +1,47 @@
-//apna college  
-//similar to 01 knapsack 
-// maam ne iss questtion ko boolean me solve karaya hai
-
-// s1-s2=target
-// s1+s2=totalSum
-// add both equation
-// (s1-s2)+(s1+s2)=target+totalSum
-// s1-s2+s1+s2=target+totalSum
-// 2s1=target+totalSum
-// s1=(target+totalSum)/2
-
-
+//study placement recursion +memoised code hai apna college ka 
 class Solution {
+    Integer[][] dp;
+
     public int findTargetSumWays(int[] nums, int target) {
-        
-        int n = nums.length;
 
-        // Step 1: Calculate total sum of array
-        int totalSum = 0;
-        for(int i = 0; i < n; i++){
-            totalSum += nums[i];
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
 
-        // Step 2: Check edge cases
-        // Agar target achieve hi nahi ho sakta
-        if(totalSum < Math.abs(target)){
+        if (((target + sum) % 2 != 0) || (Math.abs(target) > sum)) {
             return 0;
         }
 
-        // Agar (target + totalSum) odd hai → partition possible nahi
-        if((target + totalSum) % 2 != 0){
-            return 0;
-        }
+        int subsetSum = (target + sum) / 2;
 
-        // Step 3: Convert problem to subset sum
-        // s1 = (target + totalSum) / 2
-        int sum = (target + totalSum) / 2;
+        dp = new Integer[nums.length + 1][subsetSum + 1];
 
-        // Step 4: DP array
-        // dp[i][j] = number of ways to get sum j using first i elements
-        int dp[][] = new int[n + 1][sum + 1];
+        return solve(nums, subsetSum, 0);
+    }
 
-        // Step 5: Base case
-        // Sum = 0 banane ka 1 hi way hota hai (empty subset)
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 1;
-        }
+    public int solve(int[] nums, int target, int idx) {
 
-        // Step 6: Fill DP table
-        for(int i = 1; i <= n; i++){
-            for(int j = 0; j <= sum; j++){
-
-                int val = nums[i - 1]; // current element
-
-                // Case 1: include + exclude
-                if(val <= j){
-                    // include (j - val) + exclude (j)
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - val];
-                }
-                else{
-                    // Case 2: only exclude
-                    dp[i][j] = dp[i - 1][j];
-                }
+        if (idx == nums.length) {
+            if (target == 0) {
+                return 1;
+            } else {
+                return 0;
             }
         }
 
-        // Final answer: using all elements to form sum
-        return dp[n][sum];
+        if (target < 0) {
+            return 0;
+        }
+
+        if (dp[idx][target] != null) {
+            return dp[idx][target];
+        }
+
+        int include = solve(nums, target - nums[idx], idx + 1);
+        int exclude = solve(nums, target, idx + 1);
+
+        dp[idx][target] = include + exclude;
+        return dp[idx][target];
     }
 }
