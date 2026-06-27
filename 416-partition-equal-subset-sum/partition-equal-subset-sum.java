064a4fc0ee6957015483d21d3ca0a8d4529adaf5
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-06-27 12:48:35 +0530

    Time: 49 ms (49.30%) | Memory: 47.5 MB (52.10%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,35 +1,53 @@
-//memoised code ko dp me convert kiya 5 steps ke through 
-class Solution {
+//apna college
+// Similar to 0/1 Knapsack,target sum
 
+class Solution {
     public boolean canPartition(int[] nums) {
 
-        int sum = 0;
-        for (int x : nums) sum += x;
+        int n = nums.length;
 
-        if (sum % 2 != 0) return false;
+        // Step 1: Calculate total sum of array
+        int totalSum = 0;
+        for(int i = 0; i < n; i++){
+            totalSum += nums[i];
+        }
 
-        int target = sum / 2;
+        // Step 2: Agar total sum odd hai to equal partition possible nahi
+        if(totalSum % 2 != 0){
+            return false;
+        }
 
-        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+        // Step 3: Target subset sum
+        int sum = totalSum / 2;
 
-        for (int i = 0; i <= nums.length; i++) {
+        // Step 4: DP array
+        // dp[i][j] = using first i elements, can we make sum j?
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+
+        // Step 5: Base case
+        // Sum = 0 hamesha possible hai (empty subset)
+        for(int i = 0; i <= n; i++){
             dp[i][0] = true;
         }
 
-        for (int index = nums.length - 1; index >= 0; index--) {
+        // Step 6: Fill DP table
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= sum; j++){
 
-            for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
+                int val = nums[i - 1];
 
-                boolean take = false;
-                if (currentTarget - nums[index] >= 0)
-                    take = dp[index + 1][currentTarget - nums[index]];
-
-                boolean notTake = dp[index + 1][currentTarget];
-
-                dp[index][currentTarget] = take || notTake;
+                // Include + Exclude
+                if(val <= j){
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - val];
+                }
+                else{
+                    // Only Exclude
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
 
-        return dp[0][target];
+        // Final answer
+        return dp[n][sum];
     }
 }
