e640685c47d53ea8fc9aa5ca2afc2d42c284fd21
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-06-28 03:48:34 +0530

    Time: 63 ms (95.57%) | Memory: 92 MB (91.78%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,24 +1,60 @@
-//neetcode150 BY destination FANG VIDEO 1
+// ANURAG CODES (Car Fleet)
+
+class Car {
+    int position;
+    double timeToTarget;
+
+    Car(int position, double timeToTarget) {
+        this.position = position;
+        this.timeToTarget = timeToTarget;
+    }
+}
+
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int n=position.length;
-        double[][] cars=new double[n][2];
 
-        for(int i=0;i<n;i++){
-            cars[i][0]=position[i];
-            cars[i][1]=(double)(target-position[i])/speed[i];
+        int n = speed.length;
+
+        //eadge case agar empty mill gaya speed and position array
+        if(n==0){
+            return 0;
         }
 
-        Arrays.sort(cars,(a,b)->Double.compare(b[0],a[0]));
+        // Step 1: Har car ke liye position aur target tak pahunchne ka time store karo
+        Car[] cars = new Car[n];
 
-        int count=0;
-        double prevTime=0;
-        for(double[] car: cars){
-            if(car[1]>prevTime){
-            count++;
-            prevTime=car[1];
+        for (int i = 0; i < n; i++) {
+            cars[i] = new Car(position[i], (target - position[i]) * 1.0 / speed[i]);
+        }
+
+        // Step 2: Cars ko position ke according sort karo (left -> right)
+        Arrays.sort(cars, new Comparator<Car>() {
+            public int compare(Car a, Car b) {
+                return a.position - b.position;
+            }
+        });
+
+        int ans = 0;
+
+        // Step 3: Rightmost car se traversal start karo
+        // Kyuki target ke sabse paas wali car kisi aur ko join nahi karegi
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Agar current car ko target pahunchne me jyada time lag raha hai,
+            // to piche wali car ise catch kar legi.
+            // Dono ek fleet ban jayengi.
+            if (cars[i].timeToTarget >= cars[i - 1].timeToTarget) {
+                cars[i - 1] = cars[i];
+            }
+
+            // Warna piche wali car current fleet ko catch nahi kar payegi.
+            // Current car ek alag fleet hai.
+            else {
+                ans++;
             }
         }
-        return count;
+
+        // Step 4: Last fleet ko count karne ke liye +1
+        return ans + 1;
     }
 }
