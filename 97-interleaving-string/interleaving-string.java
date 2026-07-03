3fef9351d78e2eda16a9b5333792cd03e7d938c6
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-07-03 18:46:05 +0530

    Time: 214 ms (6.21%) | Memory: 282.5 MB (5.00%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,63 @@
+//code story with mik aproach 1  with 3 variable 
+class Solution {
+
+    int m, n, N;
+    int[][][] dp;
+
+    public boolean solve(int i, int j, int k, String s1, String s2, String s3) {
+
+        if (i == m && j == n && k == N) {
+            return true;
+        }
+
+        if (k >= N) { // other string didn't get consumed completely
+            return false;
+        }
+
+        if (dp[i][j][k] != -1) {
+            return dp[i][j][k] == 1;
+        }
+
+        boolean result = false;
+
+        if (i < m && s1.charAt(i) == s3.charAt(k)) {
+            result = solve(i + 1, j, k + 1, s1, s2, s3);
+        }
+
+        if (result) {
+            dp[i][j][k] = 1;
+            return true;
+        }
+
+        if (j < n && s2.charAt(j) == s3.charAt(k)) {
+            result = solve(i, j + 1, k + 1, s1, s2, s3);
+        }
+
+        if (result) {
+            dp[i][j][k] = 1;
+        } else {
+            dp[i][j][k] = 0;
+        }
+
+        return result;
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
+
+        dp = new int[101][101][201];
+
+        for (int i = 0; i <= 100; i++) {
+            for (int j = 0; j <= 100; j++) {
+                for (int k = 0; k <= 200; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+
+        return solve(0, 0, 0, s1, s2, s3);
+    }
+}
