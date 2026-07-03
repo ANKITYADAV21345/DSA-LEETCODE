89997baa7c43cb7a5d30871cc3a3f95fee961993
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-07-03 18:49:03 +0530

    Time: 4 ms (55.10%) | Memory: 46.6 MB (7.42%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,42 +1,46 @@
-//code story with mik aproach 1  with 3 variable 
+//code story with mik
 class Solution {
 
     int m, n, N;
-    int[][][] dp;
+    int[][] dp;
 
-    public boolean solve(int i, int j, int k, String s1, String s2, String s3) {
+    public boolean solve(int i, int j, String s1, String s2, String s3) {
 
-        if (i == m && j == n && k == N) {
+        if (i == m && j == n && i + j == N) {
             return true;
         }
 
-        if (k >= N) { // other string didn't get consumed completely
+        if (i + j >= N) { // other string didn't get consumed completely
             return false;
         }
 
-        if (dp[i][j][k] != -1) {
-            return dp[i][j][k] == 1;
+        if (dp[i][j] != -1) {
+            if (dp[i][j] == 1) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
         boolean result = false;
 
-        if (i < m && s1.charAt(i) == s3.charAt(k)) {
-            result = solve(i + 1, j, k + 1, s1, s2, s3);
+        if (i < m && s1.charAt(i) == s3.charAt(i + j)) {
+            result = solve(i + 1, j, s1, s2, s3);
         }
 
         if (result) {
-            dp[i][j][k] = 1;
+            dp[i][j] = 1;
             return true;
         }
 
-        if (j < n && s2.charAt(j) == s3.charAt(k)) {
-            result = solve(i, j + 1, k + 1, s1, s2, s3);
+        if (j < n && s2.charAt(j) == s3.charAt(i + j)) {
+            result = solve(i, j + 1, s1, s2, s3);
         }
 
         if (result) {
-            dp[i][j][k] = 1;
+            dp[i][j] = 1;
         } else {
-            dp[i][j][k] = 0;
+            dp[i][j] = 0;
         }
 
         return result;
@@ -48,16 +52,14 @@ class Solution {
         n = s2.length();
         N = s3.length();
 
-        dp = new int[101][101][201];
+        dp = new int[101][101];
 
         for (int i = 0; i <= 100; i++) {
             for (int j = 0; j <= 100; j++) {
-                for (int k = 0; k <= 200; k++) {
-                    dp[i][j][k] = -1;
-                }
+                dp[i][j] = -1;
             }
         }
 
-        return solve(0, 0, 0, s1, s2, s3);
+        return solve(0, 0, s1, s2, s3);
     }
 }
