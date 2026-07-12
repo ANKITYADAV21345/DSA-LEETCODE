4a2e3dd5cec502a782dbd6678660557df77040a4
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-07-12 16:24:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.46%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,44 +1,46 @@
-//apnacollege yt 
+//apna college
 class Solution {
-
-    boolean searchinrow(int[][] matrix, int target, int row) {
-        int n = matrix[0].length;
-        int start = 0;
-        int end = n - 1;
-
-        while (start <= end) {   // Fixed
-            int mid = start + (end - start) / 2;
-
-            if (target == matrix[row][mid]) {
-                return true;
-            } else if (target > matrix[row][mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return false;
-    }
-
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int rows=matrix.length;
+        int cols=matrix[0].length;
 
-        int startrow = 0;
-        int endrow = m - 1;
+        int i=0,j=rows*cols-1;
 
-        while (startrow <= endrow) {
-            int midrow = startrow + (endrow - startrow) / 2;
+        while(i<=j){
+            
 
-            if (target >= matrix[midrow][0] && target <= matrix[midrow][n - 1]) {
-                return searchinrow(matrix, target, midrow);
-            } else if (target > matrix[midrow][n - 1]) {   // Fixed
-                startrow = midrow + 1;
-            } else {
-                endrow = midrow - 1;
-            }
+            //ise feel ayegi ki kyu row=mid/cols and col=mid%cols
+            // Index   Row   Col    Value
+
+            // 0       0     0       1
+            // 1       0     1       3
+            // 2       0     2       5
+            // 3       0     3       7
+
+            // 4       1     0      10
+            // 5       1     1      11
+            // 6       1     2      16
+            // 7       1     3      20
+
+            // 8       2     0      23
+            // 9       2     1      30
+            // 10      2     2      34
+            // 11      2     3      60
+            int mid=(i+j)/2;
+            int row=mid/cols;
+            int col=mid%cols;
+            
+            if(matrix[row][col]==target)
+            //found the row so apply binary search in this perticular row
+            return true;
+
+            else if(matrix[row][col]<target)
+            //move downward in matrix from mid
+            i=mid+1;
+            else
+            //move upward in matrix row from mid
+            j=mid-1;
         }
-
         return false;
     }
 }
