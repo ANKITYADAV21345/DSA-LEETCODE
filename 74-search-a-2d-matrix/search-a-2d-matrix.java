eb51de13b79a3ff07ac79605f913b2243dd54650
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-07-12 16:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (24.46%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,27 +1,44 @@
-//apna college
+//apnacollege yt 
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int rows=matrix.length;
-        int cols=matrix[0].length;
 
-        int i=0,j=rows*cols-1;
+    boolean searchinrow(int[][] matrix, int target, int row) {
+        int n = matrix[0].length;
+        int start = 0;
+        int end = n - 1;
 
-        while(i<=j){
-            int mid=(i+j)/2;
-            int row=mid/cols;
-            int col=mid%cols;
-            
-            if(matrix[row][col]==target)
-            //found the row so apply binary search in this perticular row
-            return true;
+        while (start <= end) {   // Fixed
+            int mid = start + (end - start) / 2;
 
-            else if(matrix[row][col]<target)
-            //move downward in matrix from mid
-            i=mid+1;
-            else
-            //move upward in matrix row from mid
-            j=mid-1;
+            if (target == matrix[row][mid]) {
+                return true;
+            } else if (target > matrix[row][mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
+        return false;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int startrow = 0;
+        int endrow = m - 1;
+
+        while (startrow <= endrow) {
+            int midrow = startrow + (endrow - startrow) / 2;
+
+            if (target >= matrix[midrow][0] && target <= matrix[midrow][n - 1]) {
+                return searchinrow(matrix, target, midrow);
+            } else if (target > matrix[midrow][n - 1]) {   // Fixed
+                startrow = midrow + 1;
+            } else {
+                endrow = midrow - 1;
+            }
+        }
+
         return false;
     }
 }
