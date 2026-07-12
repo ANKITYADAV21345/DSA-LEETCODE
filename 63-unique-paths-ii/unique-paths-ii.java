cd3dbd3d4a86b552423e6bd1f92d1845431378b4
ANKITYADAV21345 <ankityadav21345@gmail.com>
2026-07-12 11:18:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (80.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,52 +1,64 @@
-// solve(i, j) → number of unique paths to reach destination (m-1, n-1)
-//              from cell (i, j)
+//code bix ke unique path 1 vala aproach lagaya and last col and last row 
+//ke teeno case jo iss question ke according the vo handle kiye 
 
-//recursion+memoisation
 class Solution {
-    int[][] memo;
-    
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        
-        memo = new int[m][n];
-        for(int[] row : memo){
-            Arrays.fill(row, -1);
+
+        // Agar destination hi blocked hai to koi path possible nahi
+        if (obstacleGrid[m - 1][n - 1] == 1)
+            return 0;
+
+        int[][] dp = new int[m][n];
+
+        // -------- Fill Last Column --------
+        // Sirf niche move kar sakte hain.
+        // Obstacle mila to uske upar wale sab cells bhi 0 ho jayenge.
+        for (int i = m - 1; i >= 0; i--) {
+
+            if (obstacleGrid[i][n - 1] == 1)
+                dp[i][n - 1] = 0;          // agar ith cel pe pathar hai to uske uper vale sel target tak nahi pahuch sakte 
+
+            else if (i == m - 1)
+                dp[i][n - 1] = 1;          // last index se last index pe pahuchne ka 1 tarika hi hota hai
+
+            else
+                dp[i][n - 1] = dp[i + 1][n - 1];   // Neeche vale cell ke barabar possible ways 
         }
-        
-        return solve(obstacleGrid, 0, 0);
-    }
-    
-    private int solve(int[][] grid, int i, int j) {
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        // Out of bounds
-        if(i >= m || j >= n)
-            return 0;
-        
-        // If obstacle found
-        if(grid[i][j] == 1)
-            return 0;
-        
-        // Reached destination
-        if(i == m - 1 && j == n - 1)
-            return 1;
-        
-        // Memoization check
-        if(memo[i][j] != -1)
-            return memo[i][j];
-        
-        // Go right + Go down
-        int right = solve(grid, i, j + 1);
-        int down = solve(grid, i + 1, j);
-        
-        return memo[i][j] = right + down;
+
+        // -------- Fill Last Row --------
+        // Sirf right move kar sakte hain.
+        // Obstacle mila to uske left wale sab cells bhi 0 ho jayenge.
+        for (int j = n - 1; j >= 0; j--) {
+
+            if (obstacleGrid[m - 1][j] == 1)
+                dp[m - 1][j] = 0;          // agar ith cel pe pathar hai to uske left vale sel target tak nahi pahuch sakte 
+
+            else if (j == n - 1)
+                dp[m - 1][j] = 1;          // Destination
+
+            else
+                dp[m - 1][j] = dp[m - 1][j + 1];   //  right vale cell ke barabar possible ways 
+        }
+
+        // -------- Fill Remaining Cells --------
+        // Path = Down + Right
+        // Obstacle hai to path = 0
+        for (int i = m - 2; i >= 0; i--) {
+
+            for (int j = n - 2; j >= 0; j--) {
+
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+
+                else
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0];
     }
 }
-
-
-
-
-
-
